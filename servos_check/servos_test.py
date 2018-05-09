4428db0f93b36c519219736283f2a8d1b1795084
= <=>
2018-05-09 14:26:35 +0700

    Added the possibility to scan and change limit angles

--- servos_check/servos_test.py
+++ servos_check/servos_test.py
@@ -1,6 +1,8 @@
 from pyax12.connection import Connection
 
 def disp_menu():
+    global servos
+    
     print("\nAvailable servos: ",end="")
     servosIDs = []
     for i in range(254):
@@ -17,6 +19,9 @@ def disp_menu():
     print('2: Set new ID')
     print('3: Set servo speed')
     print('4: Print control table')
+    print('5: Set baudrate 57600')
+    print('6: New scan')
+    print('7: Set angle limit')
     print('q: Quit')
     
     choice = input('    Waiting for input: ')
@@ -63,7 +68,37 @@ def disp_menu():
             servos.pretty_print_control_table(ID)
         else:
             print('    Wrong ID!')
-    
+
+    elif choice == '5':
+        for i in range(256):
+            custom_bd = 2000000 / (i+1)
+            servos_custom_br = Connection(port="/dev/ttyAMA0", baudrate=custom_bd, rpi_gpio=True)
+            servos_custom_br.set_baud_rate(254,57.600)
+        servos = Connection(port="/dev/ttyAMA0", baudrate=57600, rpi_gpio=True)
+
+    elif choice == '6':
+        pass
+
+    elif choice == '7':
+        choice = input('    Select a servo (254 for broadcast): ')
+        ID = int(choice)
+        if (ID in servosIDs) or ID == 254:
+            choice = input('    CW angle limit: ')
+            cw_angle_limit = int(choice)
+            if abs(cw_angle_limit) <= 150: 
+                servos.set_cw_angle_limit(ID,cw_angle_limit,degrees=True)
+            else:
+                print('    Wrong angle limit (-150 to 150)!')
+
+            choice = input('    CCW angle limit: ')
+            ccw_angle_limit = int(choice)
+            if abs(ccw_angle_limit) <= 150: 
+                servos.set_ccw_angle_limit(ID,ccw_angle_limit,degrees=True)
+            else:
+                print('    Wrong angle limit (-150 to 150)!')
+            
+    else:
+        print('    Wrong input!')
     return False
                 
             
@@ -71,5 +106,9 @@ def disp_menu():
 servos = Connection(port="/dev/ttyAMA0", baudrate=57600, rpi_gpio=True)
 end = False
 while end == False:
-    end = disp_menu()
+    try:
+        end = disp_menu()
+    except:
+        print("An error has occured!")
+        pass
 servos.close()
