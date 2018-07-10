0e0e941ef2e2f3227946c876f2e008c713a53038
Vincema <maire.vincent31@gmail.com>
2018-07-10 09:54:01 +0700

    Adding infinite loop if cannot connect to the bbserial

--- bb_serial.py
+++ bb_serial.py
@@ -1,31 +1,37 @@
 import pigpio
 import subprocess
 import constants
+import time
 
 class bitbang_serial:
     
     def __init__(self, RX, TX, baudr):
-        subprocess.call(["sudo", "pigpiod"])
-        self.pi_gpio_handler = pigpio.pi()
-        self.RX_pin = RX
-        self.TX_pin = TX
-        self.baudrate = baudr
-        self.pi_gpio_handler.set_mode(self.RX_pin, pigpio.INPUT)
-        self.pi_gpio_handler.set_mode(self.TX_pin, pigpio.OUTPUT)
-        
-        pigpio.exceptions = False
-        self.pi_gpio_handler.bb_serial_read_close(self.RX_pin) # In case the port is alrdy open
-        pigpio.exceptions = True
+        while True:
+            try:
+                subprocess.call(["sudo", "pigpiod"])
+                self.pi_gpio_handler = pigpio.pi()
+                self.RX_pin = RX
+                self.TX_pin = TX
+                self.baudrate = baudr
+                self.pi_gpio_handler.set_mode(self.RX_pin, pigpio.INPUT)
+                self.pi_gpio_handler.set_mode(self.TX_pin, pigpio.OUTPUT)
+                
+                pigpio.exceptions = False
+                self.pi_gpio_handler.bb_serial_read_close(self.RX_pin) # In case the port is alrdy open
+                pigpio.exceptions = True
 
-        # Open serial
-        self.pi_gpio_handler.bb_serial_read_open(self.RX_pin, self.baudrate)   
+                # Open serial
+                self.pi_gpio_handler.bb_serial_read_open(self.RX_pin, self.baudrate)   
+                break
+            except:
+                time.sleep(1)
+                pass
 
     def close_bb_serial(self):
         self.pi_gpio_handler.bb_serial_read_close(self.RX_pin)
         self.pi_gpio_handler.stop()
 
     def read_line_bb_serial(self):
-        #try:
         str_line = ""
         end = False
         while end != True:
@@ -33,7 +39,6 @@ class bitbang_serial:
             if count: 
                 input_str = input_bytes.decode("utf-8")
                 for i in range(count):
-                    #print(input_bytes[i])
                     if input_bytes[i] == 13: # Enter
                         end = True
                         break
@@ -47,8 +52,6 @@ class bitbang_serial:
                         str_line += input_str[i]
                         self.write_line_bb_serial(input_str[i],False)
         return str_line
-        #except:
-        #    cust_print("Bitbang serial error!")
 
     def read_bb_serial(self):
         return self.pi_gpio_handler.bb_serial_read(self.RX_pin)
